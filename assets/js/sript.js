0a78db82328d3c1ebe12a4b142065c2914c7ef46
Ort <orthodoxplus@yandex.ru>
2021-07-21 17:26:28 +0300

    added js

---
+++ assets/js/sript.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const csv = document.getElementById('csv')
+    const sql = document.getElementById('sql')
+
+    csv.addEventListener("keyup", function (event) {
+        CSVKeyupTimeout(csv, sql)
+    })
+    sql.addEventListener("click", function (event) {
+        sql.select()
+        document.execCommand("copy");
+    })
+});
+function CSVKeyupTimeout(csv, sql) {
+    setTimeout(function () {
+        sql.value = CSVToSQL(csv)
+    }, 700);
+}
+function CSVToSQL(CSV) {
+    CSV.disabled = true
+    const csvTextArray = CSV.value.replaceAll('\r\n','\n').split('\n')
+    let header = csvTextArray.shift()
+    if (header.includes(',')){
+        throw 'not valid CSV (can be just one column)'
+    }
+    let listElementsString = ''
+    csvTextArray.forEach(function (row) {
+        if (row.includes(',')){
+            throw 'not valid CSV (can be just one column)'
+        }
+        if (row === ''){
+            /* skip iteration forEach */
+            return true
+        }
+        listElementsString += row
+        listElementsString += ', '
+    })
+    listElementsString = listElementsString.slice(0, listElementsString.length - 2)
+    listElementsString = ' in (' + listElementsString + ')'
+    console.log(header + listElementsString)
+    return header + listElementsString
+}
